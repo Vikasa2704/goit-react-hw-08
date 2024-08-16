bff6c73a961cc6fb8e5fd2d7f51295aefbab2aa5
Vikasa2704 <vikasa2704@gmail.com>
2024-08-16 17:52:43 +0300

    Finish

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,21 @@
 // import img from '../../img/PhoneBook.jpg'
 import css from './HomePage.module.css'
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
+import { useSelector } from 'react-redux';
 
 
 
 const HomePage = () => {
+  
+    const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <div className={css.container}>
         <h1 className={css.title}>Welcome to Your Personal Phone Book! 📝 </h1>
  {/* <img src={img} alt="Phone Book" /> */}
-					<p className={css.text}>Please register to start saving your contacts!</p>
+  {!isLoggedIn && (
+      <p className={css.text}>Please register to start saving your contacts!</p>
+      )}
+					
     </div>
   )
 }
